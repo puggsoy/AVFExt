9a3dbce832b7c1988efdb877ee78ec727841ec7a
Sami Habib <puggsoy@gmail.com>
2025-10-26 20:01:48 +1300

    Tidy up unused usings and switch from spaces to tabs

--- AVFExt/AVF.cs
+++ AVFExt/AVF.cs
@@ -1,147 +1,142 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 namespace AVFExt
 {
-    class AVF
-    {
-        static public AVF load(FileStream f, string fName)
-        {
-            BinaryReader br = new BinaryReader(f);
-            string magic = new string(br.ReadChars(0x0F));
+	class AVF
+	{
+		static public AVF load(FileStream f, string fName)
+		{
+			BinaryReader br = new BinaryReader(f);
+			string magic = new string(br.ReadChars(0x0F));
 
-            if(magic != "AVF WayneSikes\0")
-            {
-                throw new Exception("Invalid AVF ID: " + magic);
-            }
+			if(magic != "AVF WayneSikes\0")
+			{
+				throw new Exception("Invalid AVF ID: " + magic);
+			}
 
-            br.BaseStream.Seek(0x15, SeekOrigin.Begin);
+			br.BaseStream.Seek(0x15, SeekOrigin.Begin);
 
-            ushort numEntries = br.ReadUInt16();
-            ushort width = br.ReadUInt16();
-            ushort height = br.ReadUInt16();
+			ushort numEntries = br.ReadUInt16();
+			ushort width = br.ReadUInt16();
+			ushort height = br.ReadUInt16();
 
-            br.BaseStream.Seek(0x21, SeekOrigin.Begin);
+			br.BaseStream.Seek(0x21, SeekOrigin.Begin);
 
-            AVFEntry[] entries = new AVFEntry[numEntries];
+			AVFEntry[] entries = new AVFEntry[numEntries];
 
-            for(int i = 0; i < numEntries; i++)
-            {
-                AVFEntry e = new AVFEntry();
-                e.frameNo = br.ReadUInt16();
-                e.offset = br.ReadUInt32();
-                e.length = br.ReadUInt32();
-                e.unk = br.ReadUInt32();
-                br.BaseStream.Seek(5, SeekOrigin.Current);
+			for(int i = 0; i < numEntries; i++)
+			{
+				AVFEntry e = new AVFEntry();
+				e.frameNo = br.ReadUInt16();
+				e.offset = br.ReadUInt32();
+				e.length = br.ReadUInt32();
+				e.unk = br.ReadUInt32();
+				br.BaseStream.Seek(5, SeekOrigin.Current);
 
-                entries[i] = e;
-            }
+				entries[i] = e;
+			}
 
-            AVF avf = new AVF();
-            avf.basename = Path.GetFileNameWithoutExtension(fName);
-            avf.frames = new Bitmap[numEntries];
-            avf.rawFrames = new byte[numEntries][];
+			AVF avf = new AVF();
+			avf.basename = Path.GetFileNameWithoutExtension(fName);
+			avf.frames = new Bitmap[numEntries];
+			avf.rawFrames = new byte[numEntries][];
 
-            for (int i = 0; i < numEntries; i++)
-            {
-                br.BaseStream.Seek(entries[i].offset, SeekOrigin.Begin);
-                byte[] outData = br.ReadBytes((int)entries[i].length);
-                outData = decrypt(outData);
+			for (int i = 0; i < numEntries; i++)
+			{
+				br.BaseStream.Seek(entries[i].offset, SeekOrigin.Begin);
+				byte[] outData = br.ReadBytes((int)entries[i].length);
+				outData = decrypt(outData);
 
-                try
-                {
-                    outData = LZSS.dec(outData);
-                }
-                catch(Exception ex)
-                {
-                    throw new Exception("Decompression issue: " + ex.ToString());
-                }
+				try
+				{
+					outData = LZSS.dec(outData);
+				}
+				catch(Exception ex)
+				{
+					throw new Exception("Decompression issue: " + ex.ToString());
+				}
 
-                avf.rawFrames[entries[i].frameNo] = new byte[outData.Length];
-                outData.CopyTo(avf.rawFrames[entries[i].frameNo], 0);
+				avf.rawFrames[entries[i].frameNo] = new byte[outData.Length];
+				outData.CopyTo(avf.rawFrames[entries[i].frameNo], 0);
 
-                Bitmap bmp = toBitmap(outData, width, height);
-                avf.frames[entries[i].frameNo] = bmp;
-            }
+				Bitmap bmp = toBitmap(outData, width, height);
+				avf.frames[entries[i].frameNo] = bmp;
+			}
 
-            return avf;
-        }
+			return avf;
+		}
 
-        static private Bitmap toBitmap(byte[] data, uint width, uint height)
-        {
-            Bitmap bmp = new Bitmap((int)width, (int)height, PixelFormat.Format16bppRgb555);
+		static private Bitmap toBitmap(byte[] data, uint width, uint height)
+		{
+			Bitmap bmp = new Bitmap((int)width, (int)height, PixelFormat.Format16bppRgb555);
 
-            BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),
-                                              ImageLockMode.ReadWrite, bmp.PixelFormat);
+			BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),
+											  ImageLockMode.ReadWrite, bmp.PixelFormat);
 
-            /*int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
-            data = new byte[bytes];
+			/*int bytes = Math.Abs(bmpData.Stride) * bmp.Height;
+			data = new byte[bytes];
 
-            for (int counter = 0; counter < data.Length; counter++)
-            {
-                if (counter % 2 == 0)
-                    data[counter] = 0;
-                else
-                    data[counter] = 0x1F;
-            }*/
+			for (int counter = 0; counter < data.Length; counter++)
+			{
+				if (counter % 2 == 0)
+					data[counter] = 0;
+				else
+					data[counter] = 0x1F;
+			}*/
 
-            IntPtr ptr = bmpData.Scan0;
+			IntPtr ptr = bmpData.Scan0;
 
-            Marshal.Copy(data, 0, ptr, data.Length);
+			Marshal.Copy(data, 0, ptr, data.Length);
 
-            bmp.UnlockBits(bmpData);
+			bmp.UnlockBits(bmpData);
 
-            return bmp;
-        }
+			return bmp;
+		}
 
-        static private byte[] decrypt(byte[] enc)
-        {
-            byte[] dec = new byte[enc.Length];
+		static private byte[] decrypt(byte[] enc)
+		{
+			byte[] dec = new byte[enc.Length];
 
-            for(int i = 0; i < enc.Length; i++)
-            {
-                dec[i] = (byte)(enc[i] - i);
-            }
+			for(int i = 0; i < enc.Length; i++)
+			{
+				dec[i] = (byte)(enc[i] - i);
+			}
 
-            return dec;
-        }
-        //End of static functions
+			return dec;
+		}
+		//End of static functions
 
-        public string basename;
-        public byte[][] rawFrames;
-        public Bitmap[] frames;
-        
-        public AVF() {}
+		public string basename;
+		public byte[][] rawFrames;
+		public Bitmap[] frames;
+		
+		public AVF() {}
 
-        public void saveFrames(string dir)
-        {
-            for(int i = 0; i < frames.Length; i++)
-            {
-                string name = basename + "_" + i + ".png";
-                frames[i].Save(Path.Combine(dir, name), ImageFormat.Png);
-            }
-        }
+		public void saveFrames(string dir)
+		{
+			for(int i = 0; i < frames.Length; i++)
+			{
+				string name = basename + "_" + i + ".png";
+				frames[i].Save(Path.Combine(dir, name), ImageFormat.Png);
+			}
+		}
 
-        public void dumpFrame(string dir, int index)
-        {
-            string name = basename + "_" + index + ".dmp";
-            File.WriteAllBytes(Path.Combine(dir, name), rawFrames[index]);
-        }
+		public void dumpFrame(string dir, int index)
+		{
+			string name = basename + "_" + index + ".dmp";
+			File.WriteAllBytes(Path.Combine(dir, name), rawFrames[index]);
+		}
 
-        private struct AVFEntry
-        {
-            public ushort frameNo;
-            public uint offset;
-            public uint length;
-            public uint unk;
-        }
-    }
+		private struct AVFEntry
+		{
+			public ushort frameNo;
+			public uint offset;
+			public uint length;
+			public uint unk;
+		}
+	}
 }
--- AVFExt/MainWindow.xaml.cs
+++ AVFExt/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.IO;
 using Microsoft.Win32;
 using Ookii.Dialogs.Wpf;
@@ -21,164 +11,164 @@ using System.Collections.ObjectModel;
 
 namespace AVFExt
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        private AVF avfFile;
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private AVF avfFile;
 
-        private const String InitialTitle = "AVFExt";
+		private const String InitialTitle = "AVFExt";
 
-        public ObservableCollection<FrameItem> frameItems = new ObservableCollection<FrameItem>();
+		public ObservableCollection<FrameItem> frameItems = new ObservableCollection<FrameItem>();
 
-        public MainWindow()
-        {
-            InitializeComponent();
-            
-            listBox.ItemsSource = frameItems;
-            listBox.DisplayMemberPath = "name";
-            listBox.SelectedValuePath = "bmp";
-            listBox.SelectionMode = SelectionMode.Single;
+		public MainWindow()
+		{
+			InitializeComponent();
+			
+			listBox.ItemsSource = frameItems;
+			listBox.DisplayMemberPath = "name";
+			listBox.SelectedValuePath = "bmp";
+			listBox.SelectionMode = SelectionMode.Single;
 
 #if DEBUG
-            dumpBtn.Visibility = Visibility.Visible;
+			dumpBtn.Visibility = Visibility.Visible;
 #endif
-        }
+		}
 
-        private void openBtn_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog d = new OpenFileDialog();
-            d.DefaultExt = "*.avf";
-            d.Filter = "AVF Files (*.avf)|*.avf";
+		private void openBtn_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog d = new OpenFileDialog();
+			d.DefaultExt = "*.avf";
+			d.Filter = "AVF Files (*.avf)|*.avf";
 
-            if (d.ShowDialog() == true)
-            {
-                openFile(d.FileName, d.SafeFileName);
-            }
-        }
-        
-        private void openFile(string fName, string safeName)
-        {
-            FileStream f = null;
+			if (d.ShowDialog() == true)
+			{
+				openFile(d.FileName, d.SafeFileName);
+			}
+		}
+		
+		private void openFile(string fName, string safeName)
+		{
+			FileStream f = null;
 
-            try
-            {
-                f = new FileStream(fName, FileMode.Open, FileAccess.Read);
+			try
+			{
+				f = new FileStream(fName, FileMode.Open, FileAccess.Read);
 
-                avfFile = AVF.load(f, safeName);
+				avfFile = AVF.load(f, safeName);
 
-                frameItems.Clear();
+				frameItems.Clear();
 
-                for(int i = 0; i < avfFile.frames.Length; i++)
-                {
-                    FrameItem fi = new FrameItem();
-                    fi.name = "Frame " + i;
-                    fi.bmp = avfFile.frames[i];
-                    frameItems.Add(fi);
-                }
+				for(int i = 0; i < avfFile.frames.Length; i++)
+				{
+					FrameItem fi = new FrameItem();
+					fi.name = "Frame " + i;
+					fi.bmp = avfFile.frames[i];
+					frameItems.Add(fi);
+				}
 
-                listBox.SelectedIndex = 0;
+				listBox.SelectedIndex = 0;
 
-                Title = InitialTitle + " - " + safeName;
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show(ex.Message, "File loading failed", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-            finally
-            {
-                if (f != null) f.Close();
-            }
-        }
+				Title = InitialTitle + " - " + safeName;
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.Message, "File loading failed", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+			finally
+			{
+				if (f != null) f.Close();
+			}
+		}
 
-        private void extractBtn_Click(object sender, RoutedEventArgs e)
-        {
-            VistaFolderBrowserDialog d = new VistaFolderBrowserDialog();
-            
-            if(d.ShowDialog() == true)
-            {
-                extractFiles(d.SelectedPath);
-            }
-        }
+		private void extractBtn_Click(object sender, RoutedEventArgs e)
+		{
+			VistaFolderBrowserDialog d = new VistaFolderBrowserDialog();
+			
+			if(d.ShowDialog() == true)
+			{
+				extractFiles(d.SelectedPath);
+			}
+		}
 
-        private void extractFiles(string outDir)
-        {
-            try
-            {
-                avfFile.saveFrames(outDir);
+		private void extractFiles(string outDir)
+		{
+			try
+			{
+				avfFile.saveFrames(outDir);
 
-                MessageBox.Show("Extracted all images to " + outDir + "!", "Extraction successful", MessageBoxButton.OK);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Extraction failed", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
+				MessageBox.Show("Extracted all images to " + outDir + "!", "Extraction successful", MessageBoxButton.OK);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Extraction failed", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+		}
 
-        private void dumpBtn_Click(object sender, RoutedEventArgs e)
-        {
-            VistaFolderBrowserDialog d = new VistaFolderBrowserDialog();
+		private void dumpBtn_Click(object sender, RoutedEventArgs e)
+		{
+			VistaFolderBrowserDialog d = new VistaFolderBrowserDialog();
 
-            if (d.ShowDialog() == true)
-            {
-                dumpFiles(d.SelectedPath);
-            }
-        }
+			if (d.ShowDialog() == true)
+			{
+				dumpFiles(d.SelectedPath);
+			}
+		}
 
-        private void dumpFiles(string outDir)
-        {
-            try
-            {
-                avfFile.dumpFrame(outDir, listBox.SelectedIndex);
+		private void dumpFiles(string outDir)
+		{
+			try
+			{
+				avfFile.dumpFrame(outDir, listBox.SelectedIndex);
 
-                MessageBox.Show("Dumped selected image to " + outDir + "!", "Dump successful", MessageBoxButton.OK);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Dump failed", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-        }
+				MessageBox.Show("Dumped selected image to " + outDir + "!", "Dump successful", MessageBoxButton.OK);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message, "Dump failed", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+		}
 
-        private void onListBoxSelectionChange(object sender, SelectionChangedEventArgs e)
-        {
-            if (listBox.Items.Count == 0)
-            {
-                extractBtn.IsEnabled = false;
-                dumpBtn.IsEnabled = false;
-                return;
-            }
+		private void onListBoxSelectionChange(object sender, SelectionChangedEventArgs e)
+		{
+			if (listBox.Items.Count == 0)
+			{
+				extractBtn.IsEnabled = false;
+				dumpBtn.IsEnabled = false;
+				return;
+			}
 
-            extractBtn.IsEnabled = true;
-            dumpBtn.IsEnabled = true;
+			extractBtn.IsEnabled = true;
+			dumpBtn.IsEnabled = true;
 
-            using (MemoryStream mem = new MemoryStream())
-            {
-                ((Bitmap)listBox.SelectedValue).Save(mem, ImageFormat.Png);
-                mem.Position = 0;
-                BitmapImage bmpImg = new BitmapImage();
-                bmpImg.BeginInit();
-                bmpImg.StreamSource = mem;
-                bmpImg.CacheOption = BitmapCacheOption.OnLoad;
-                bmpImg.EndInit();
-                image.Source = bmpImg;
-                image.Width = bmpImg.Width;
-                image.Height = bmpImg.Height;
-            }
-        }
-    }
+			using (MemoryStream mem = new MemoryStream())
+			{
+				((Bitmap)listBox.SelectedValue).Save(mem, ImageFormat.Png);
+				mem.Position = 0;
+				BitmapImage bmpImg = new BitmapImage();
+				bmpImg.BeginInit();
+				bmpImg.StreamSource = mem;
+				bmpImg.CacheOption = BitmapCacheOption.OnLoad;
+				bmpImg.EndInit();
+				image.Source = bmpImg;
+				image.Width = bmpImg.Width;
+				image.Height = bmpImg.Height;
+			}
+		}
+	}
 
-    public struct FrameItem
-    {
-        public string name
-        {
-            get;
-            set;
-        }
-        public Bitmap bmp
-        {
-            get;
-            set;
-        }
-    }
+	public struct FrameItem
+	{
+		public string name
+		{
+			get;
+			set;
+		}
+		public Bitmap bmp
+		{
+			get;
+			set;
+		}
+	}
 }
